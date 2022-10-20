2d5ae0dcf98821e756d12e5b08e3082f756b3cdc
Kate Galich <kate.galich@outlook.com>
2022-10-20 18:32:30 -0400

    prevent sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,6 +6,8 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+const cohortName = process.argv[2] || 'JUL02';
+const values = [cohortName];
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -13,10 +15,10 @@ FROM teachers
 JOIN assistance_requests ON teacher_id=teachers.id
 JOIN students ON student_id=students.id
 JOIN cohorts ON cohort_id=cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name
 ;
-`)
+`, values)
 .then(res => {
   console.log(res.rows);
 })
